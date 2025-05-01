ec226a25ea9c44ba57cba47a0b7f18547b9e651a
Taha Rashid <taharashid710192@gmail.com>
2025-05-01 08:55:05 -0400

    feat: created search and details functions

--- rawg.js
+++ rawg.js
@@ -1,4 +1,36 @@
 // Taha Rashid
 // May 1, 2025
+// RAWG API
 
+const credentials = require('./credentials');
 const axios = require('axios');
+
+// RAWG search
+// Only called when user is searching for games
+const RAWGSearch = async (search, isPrecise) => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `https://api.rawg.io/api/games?key=${credentials.RAWGKey}&search=${search}&search_precise=${isPrecise}`,
+        headers: { }
+      };
+
+      const response = await axios.request(config);
+      return response.data;
+};
+
+// RAWG game details
+// Assumes we already know gameID
+const RAWGGame = async (gameID) => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `https://api.rawg.io/api/games/${gameID}?key=${credentials.RAWGKey}`,
+        headers: { }
+      };
+
+    const response = await axios.request(config);
+    return response.data;
+}
+
+
