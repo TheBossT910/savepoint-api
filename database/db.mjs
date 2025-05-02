4b5ca0a64988a2312d99ae23d3f62a7a618f088b
Taha Rashid <taharashid710192@gmail.com>
2025-05-02 12:56:00 -0400

    feat: created fn to read data from db when given slug, upc

--- database/db.mjs
+++ database/db.mjs
@@ -8,13 +8,6 @@ import { createClient } from '@supabase/supabase-js';
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// // reading data
-const { data, errorRead } = await supabase
-  .from('games')
-  .select('data')
-
-console.log(data, errorRead);
-
 // // writing data
 // const { errorWrite } = await supabase
 //   .from('games')
@@ -31,3 +24,30 @@ console.log(data, errorRead);
 //     data: {name: 'Zain Rashid' }
 //   })
 //   .eq('id', 4);
+
+// We want to read if 1. upc, or 2. slug exists in the db. If it exists, done. If not, then we scrape via external api
+// reading data
+// slug is a string, upc is represented as an int, so we need a sentinel value of -1 if there is no upc
+const databaseRead = async (slug = '', upc = '-1') => {
+  // setting empty upc to sentinel -1 value
+  if (upc == '') upc = '-1';
+
+  const { data, errorRead } = await supabase
+    .from('games')
+    .select('data')
+    .or(`slug.eq.${slug},upc.eq.${upc}`);
+  
+  return data;
+}
+
+module.exports = { databaseRead };
+
+// testing
+// databaseRead('taha-rashid', '')
+//   .then( (res) => {
+//     if (res == null || res.length == 0) {
+//       console.log("no data");
+//     } else {
+//       console.log(res);
+//     }
+//   });
