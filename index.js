033ec8fba965f789aab81bf23126768d3c943647
Taha Rashid <taharashid710192@gmail.com>
2025-05-02 10:19:08 -0400

    chore: clean up API, return 200 message

--- index.js
+++ index.js
@@ -30,38 +30,34 @@ app.get('/games/upc/:upc', (req, res) => {
 });
 
 // get game from search, query is q
-// TODO: implement fully!
 app.get('/games/search', (req, res) => {
     let search = req.query.q;
 
     let response = api.retrieveSearch(search);
     response
-        .then( (data) => res.status(501).send( data ));
+        .then( (data) => res.status(200).send( data ));
 });
 
 // get most popular games of all time
-// TODO: implement fully!
 app.get('/games/lists/popular', (req, res) => {
     let response = api.gamesPopular();
     response
-        .then( (data) => res.status(501).send(data) );
+        .then( (data) => res.status(200).send(data) );
 });
 
 // get currently popular (trending) games
-// TODO: implement fully!
 app.get('/games/lists/trending', (req, res) => {
     let response = api.gamesTrending();
     response
-        .then( (data) => res.status(501).send(data) );
+        .then( (data) => res.status(200).send(data) );
 });
 
 // get highest (most) rated games for specified platform
-// TODO: implement fully!
 app.get('/games/lists/highest-rated', (req, res) => {
     let platform = req.query.platform
     let response = api.gamesHighestRated(platform);
     response
-        .then( (data) => res.status(501).send(data) );
+        .then( (data) => res.status(200).send(data) );
 });
 
 // get users
