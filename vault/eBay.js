6b1b6ea8f46e1c8d3a1eda9af01634b88b42b041
Taha Rashid <taharashid710192@gmail.com>
2025-05-04 15:59:48 -0400

    chore: implemented outlier filtering
    Also returned the loose, complete, new game prices

--- vault/eBay.js
+++ vault/eBay.js
@@ -4,6 +4,41 @@
 
 const { chromium } = require('playwright');
 
+// Function to filter out outliers in an array of data
+// Courtesy of StackOverflow, https://stackoverflow.com/questions/20811131/javascript-remove-outlier-from-an-array
+function filterOutliers(someArray) {  
+
+  // Copy the values, rather than operating on references to existing values
+  var values = someArray.concat();
+
+  // Then sort
+  values.sort( function(a, b) {
+          return a - b;
+       });
+
+  /* Then find a generous IQR. This is generous because if (values.length / 4) 
+   * is not an int, then really you should average the two elements on either 
+   * side to find q1.
+   */     
+  var q1 = values[Math.floor((values.length / 4))];
+  // Likewise for q3. 
+  var q3 = values[Math.ceil((values.length * (3 / 4)))];
+  var iqr = q3 - q1;
+
+  // Then find min and max values
+  var maxValue = q3 + iqr*1.5;
+  var minValue = q1 - iqr*1.5;
+
+  // Then filter anything beyond or beneath these values.
+  var filteredValues = values.filter(function(x) {
+      return (x <= maxValue) && (x >= minValue);
+  });
+
+  // Then return
+  return filteredValues;
+}
+
+
 // get prices for active/sold listings
 const eBayPrices = async (url) => {
 
@@ -76,9 +111,9 @@ const productConditions = async () => {
     await browser.close(); 
   };
 
-// TODO: Need to do math to find non-outliers and average them out to get a pricing estimate
+// TODO: need to be able to switch between the different keywords (disc+only, cartridge+only, etc.)
+// need to format numbers to pricing format
 // Won't use productConditions() for now...
-// Also need to be able to switch between the different keywords (disc+only, cartridge+only, etc.)
 const productValuation = async (upcArr) => {
   // let upc = '045496590741';  // Super Mario Odyssey
   let upc = '711719547518';     // Ghost of Tsushima
@@ -98,23 +133,29 @@ const productValuation = async (upcArr) => {
   let urlComplete = `https://www.ebay.com/sch/i.html?_nkw=${completeSearch}&_in_kw=1&LH_Sold=1&LH_Complete=1&_ipg=240&_ex_kw=${compelteExclusion}`;
   let urlNew = `https://www.ebay.com/sch/i.html?_nkw=${newSearch}&_in_kw=1&LH_Sold=1&LH_Complete=1&_ipg=240&_ex_kw=${newExclusion}`;
 
-  let resLoose = await eBayPrices(urlLoose);
-  let resComplete = await eBayPrices(urlComplete);
-  let resNew = await eBayPrices(urlNew);
+  // grabbing prices and removing outliers
+  let resLoose = filterOutliers( await eBayPrices(urlLoose) );
+  let resComplete = filterOutliers( await eBayPrices(urlComplete) );
+  let resNew = filterOutliers( await eBayPrices(urlNew) );
 
-  // console.log(res); // results
-  // console.log(res.length);  // number of results
-
+  // getting the average value
   let averageLoose = resLoose.reduce( (accumulator, currentValue) => accumulator + currentValue ) / resLoose.length;
   let averageComplete = resComplete.reduce( (accumulator, currentValue) => accumulator + currentValue ) / resComplete.length;
   let averageNew = resNew.reduce( (accumulator, currentValue) => accumulator + currentValue ) / resNew.length;
 
-  console.log(`Loose price: ${averageLoose}`);
-  console.log(`Complete price: ${averageComplete}`);
-  console.log(`New price: ${averageNew}`);
+  // console.log(`Loose price: ${averageLoose}`);
+  // console.log(`Complete price: ${averageComplete}`);
+  // console.log(`New price: ${averageNew}`);
+
+  // returning results
+  return {'loose': averageLoose, 'complete': averageComplete, 'new':averageNew};
 }
 
 // testing
 // productConditions();
-productValuation()
-
+// productValuation()
+//   .then( (res) => {
+//     console.log(`Loose price: ${res.loose}`);
+//     console.log(`Complete price: ${res.complete}`);
+//     console.log(`New price: ${res.new}`);
+//   });
