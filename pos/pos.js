48cbf4ae6695032cab54acbf67bc77107f48f23f
Taha Rashid <taharashid710192@gmail.com>
2025-05-05 20:57:52 -0400

    chore: imported env, supabase

--- pos/pos.js
+++ pos/pos.js
@@ -1,3 +1,9 @@
 // Taha Rashid
 // May 5, 2025
 // managing pos
+
+require('dotenv').config()
+const { createClient } = require('@supabase/supabase-js');
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
