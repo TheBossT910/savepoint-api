cd9b98f7daf67522985ac7e4db0b22c4f84791ee
Taha Rashid <taharashid710192@gmail.com>
2025-05-06 08:54:00 -0400

    chore: declared create, remove, get inventory functions

--- pos/pos.js
+++ pos/pos.js
@@ -7,3 +7,18 @@ const { createClient } = require('@supabase/supabase-js');
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+// add to pos
+const createInventory = async () => {
+
+};
+
+// retrieve from pos
+const getInventory = async() => {
+
+};  
+
+// remove from pos
+const removeInventory = async () => {
+
+};
